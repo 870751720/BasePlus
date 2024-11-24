19bdee8588e28174fda5ba5773a648b4239a5294
870751720 <870751720@qq.com>
2024-11-25 00:19:42 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # EasyCPlusPlus
+
+# 安装
+1 确保安装了推荐插件cmaketools
+
+2 确保安装了vs 2022,确保Visual Studio 2022中安装了"使用C++的桌面开发"工作负载
+
+3 安装vcpkg
+git clone https://github.com/Microsoft/vcpkg.git
+确保vcpkg在 C:\Program Files
+.\bootstrap-vcpkg.bat
+.\vcpkg integrate install
+
+4 使用
+在CMakeTool上,配置里面可以切debug和release
+在项目大纲上对具体的app右键设置为启动调试目标可以切换运行
--- apps/app1/src/main.cpp
+++ apps/app1/src/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main() {
     // 示例1：字符串转换
-    string text = "hello world";
+    string text = "hello cccc";
     boost::to_upper(text);
     cout << "turn da: " << text << endl;  // 输出: HELLO WORLD
 
     // 示例2：字符串修剪
-    string text2 = "   hello   ";
+    string text2 = "   helloxy ccc rrrrrrrrrrrrrrr ";
     boost::trim(text2);
     cout << "turn xiao: [" << text2 << "]" << endl;  // 输出: [hello]
 
