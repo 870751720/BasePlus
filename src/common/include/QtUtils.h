3dca6fe6251317d8182f00d544c6289db639a055
870751720 <870751720@qq.com>
2024-12-07 00:48:43 +0800

    x

--- src/common/include/QtUtils.h
+++
@@ -1,8 +0,0 @@
-#ifndef QT_UTILS_H
-#define QT_UTILS_H
-
-#include <QApplication>
-
-QApplication* createQApplication(int argc, char *argv[]);
-
-#endif // QT_UTILS_H
--- tools/initEnv.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-%1 %2
-ver|find "5.">nul&&goto :Admin
-mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof
-
-:Admin
-set /p VCPKG_PATH="请输入vcpkg的路径: "
-setx VCPKG_HOME "%VCPKG_PATH%" /M
-setx PATH "%PATH%;%VCPKG_PATH%" /M
